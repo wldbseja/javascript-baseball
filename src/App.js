3a02ab0231169d5ba42fb2cbc3118e461c236c54
JiYoon Lee <wldbseja@daum.net>
2022-12-10 14:10:57 +0900

    feat (App) :  랜덤 숫자 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
+  #computer;
   constructor() {}
 
   printGameStart() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
+  generateRandomNumbers() {
+    this.#computer = [];
+    while (this.#computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.#computer.includes(number)) {
+        this.#computer.push(number);
+      }
+    }
+  }
+
   play() {
     this.printGameStart();
+    this.generateRandomNumbers();
   }
 }
 
