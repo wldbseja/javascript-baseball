16630fb5232746685f0514fc11c6389750b7431f
JiYoon Lee <wldbseja@daum.net>
2022-12-10 17:00:53 +0900

    feat (constants) : const  파일 추가

--- src/constants.js
+++ src/constants.js
@@ -13,4 +13,9 @@ const PRINT_ERROR_STRING = {
   ERROR_RANGE: '[ERROR] 1부터 9까지의 숫자가 아니므로 프로그램이 종료 됩니다.',
 };
 
+const PRINT_RESULT_COUNT = {
+  STRIKE_COUNT: `${strikeCount}스트라이크`,
+  BALL_COUNT: `${ballCount}볼`,
+  BLANK: ' ',
+};
 module.exports = { NUMBER, PRINT_STRING, PRINT_ERROR_STRING };
