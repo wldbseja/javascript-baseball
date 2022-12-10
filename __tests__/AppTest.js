538bf2fc1ccff9b84f4186b5755e895e6a39fb04
JiYoon Lee <wldbseja@daum.net>
2022-12-10 15:48:11 +0900

    test (AppTest) : 에러테스트 - 중복 테스트

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -1,2 +1,14 @@
 const App = require('../src/App');
 const MissionUtils = require('@woowacourse/mission-utils');
+
+describe(' 에러 테스트 ', () => {
+  test('사용자 입력 값 중복 테스트', () => {
+    const app = new App();
+
+    expect(() => app.checkNumberCount([2, 2, 3])).toThrow();
+    expect(() => app.checkNumberCount([4, 5, 5])).toThrow();
+
+    expect(() => app.checkNumberCount([1, 2, 3])).not.toThrow();
+    expect(() => app.checkNumberCount([4, 5, 6])).not.toThrow();
+  });
+});
