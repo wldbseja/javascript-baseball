35619f77c7f69ec7adf4d6497f3a53820cc2c60b
JiYoon Lee <wldbseja@daum.net>
2022-12-10 15:58:54 +0900

    test (AppTest) : 에러테스트 - 범위 테스트

--- __tests__/AppTest.js
+++ __tests__/AppTest.js
@@ -21,4 +21,14 @@ describe(' 에러 테스트 ', () => {
     expect(() => app.checkNumberLength([1, 2, 3])).not.toThrow();
     expect(() => app.checkNumberLength([4, 5, 6])).not.toThrow();
   });
+
+  test('사용자 입력 값 범위 테스트', () => {
+    const app = new App();
+
+    expect(() => app.checkNumberRange([0, 2, 3])).toThrow();
+    expect(() => app.checkNumberRange([4, 5, h])).toThrow();
+
+    expect(() => app.checkNumberRange([1, 2, 3])).not.toThrow();
+    expect(() => app.checkNumberRange([4, 5, 6])).not.toThrow();
+  });
 });
